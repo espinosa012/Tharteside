171dcf5df6c8aac3c74ff730810b50135b6e9b1a
Adrián R. Espinosa <espinosa012@gmail.com>
2023-07-06 19:10:58 +0200

    borders sprites ok

--- mono/WorldManager.cs
+++ mono/WorldManager.cs
@@ -46,13 +46,23 @@ public partial class WorldManager : Node2D
 		{
 			for (var y = chunkPosition.Y * ChunkSize.Y * SquareSize.Y; y < ChunkSize.Y * SquareSize.Y + chunkPosition.Y * ChunkSize.Y * SquareSize.Y; y+=SquareSize.Y)
 			{
-				var worldPosition = new Vector2I((x/SquareSize.X)+TileMapOffset.X, (y/SquareSize.Y)+TileMapOffset.Y);
-				var valueTier = _world.GetValueTierAt(worldPosition.X, worldPosition.Y);
-
+				var squarePos = new Vector2I(x, y);
+				var valueTier = GetValueTierByWorldPos(GetWorldPosBySquare(squarePos));
 				FulfillSquare(new Vector2I(x, y), valueTier);	// escalado del mapa
 			}
 		}
 	}
+
+	private Vector2I GetWorldPosBySquare(Vector2I squarePos)
+	{
+		return new Vector2I((squarePos.X/SquareSize.X)+TileMapOffset.X, (squarePos.Y/SquareSize.Y)+TileMapOffset.Y);
+	}
+
+	private int GetValueTierByWorldPos(Vector2I worldPosition)
+	{
+		return _world.GetValueTierAt(worldPosition.X, worldPosition.Y);
+	}
+	
 	public void UpdateTileMap()
 	{
 		_tileMap.Clear();
@@ -61,51 +71,151 @@ public partial class WorldManager : Node2D
 
 	private void FulfillSquare(Vector2I worldPos, int valueTier)
 	{
-		if (!IsFrontier(worldPos))
+		var isStepDownNorth = IsStepDownNorth(worldPos, valueTier);
+		var isStepDownSouth = IsStepDownSouth(worldPos, valueTier);
+		var isStepDownWest = IsStepDownWest(worldPos, valueTier);
+		var isStepDownEast = IsStepDownEast(worldPos, valueTier);
+		
+		var isStepDownNorthWest = IsStepDownNorthWest(worldPos, valueTier);
+		var isStepDownNorthEast = IsStepDownNorthEast(worldPos, valueTier);
+		var isStepDownSouthWest = IsStepDownSouthWest(worldPos, valueTier);
+		var isStepDownSouthEast = IsStepDownSouthEast(worldPos, valueTier);
+
+		var isSideBorder = IsSideBorder(worldPos, valueTier);
+		
+		for (var i = 0; i < SquareSize.X; i++)
 		{
-			for (var i = 0; i < SquareSize.X; i++)
+			for (var j = 0; j < SquareSize.Y; j++)
 			{
-				for (var j = 0; j < SquareSize.Y; j++)
+				var tileSetSource = 5;
+				var tileSetAtlasCoordinates = new Vector2I(1, 1);
+
+				if (isStepDownNorth && j == 0)
 				{
-					SetCell(new Vector2I(worldPos.X+i, worldPos.Y+j), valueTier);	
+					tileSetAtlasCoordinates = new Vector2I(1, 3);
+					tileSetSource = 5;
 				}
-			}	
+
+				if (isStepDownSouth && j == SquareSize.Y - 1)
+				{
+					tileSetAtlasCoordinates = new Vector2I(1, 5);
+					tileSetSource = 5;
+				}
+
+				if (isStepDownWest && i == 0)
+				{
+					tileSetAtlasCoordinates = new Vector2I(0, 4);
+					tileSetSource = 5;
+					if (IsStepDownNorth(worldPos, valueTier) && j == 0)
+					{
+						tileSetAtlasCoordinates = new Vector2I(0, 3);
+					}
+					if (IsStepDownSouth(worldPos, valueTier) && j == SquareSize.Y - 1)
+					{
+						tileSetAtlasCoordinates = new Vector2I(0, 5);
+					}
+				}
+
+				if (isStepDownEast && i == SquareSize.X - 1)
+				{
+					tileSetAtlasCoordinates = new Vector2I(2, 4);
+					tileSetSource = 5;
+					if (IsStepDownNorth(worldPos, valueTier) && j == 0)
+					{
+						tileSetAtlasCoordinates = new Vector2I(2, 3);
+					}
+					if (IsStepDownSouth(worldPos, valueTier) && j == SquareSize.Y - 1)
+					{
+						tileSetAtlasCoordinates = new Vector2I(2, 5);
+					}
+				}
+
+				if (!isSideBorder)
+				{
+					if (isStepDownNorthWest && i == 0 && j == 0)
+					{
+						tileSetAtlasCoordinates = new Vector2I(3, 2);
+						tileSetSource = 5;
+					}
+				
+					if (isStepDownSouthWest && i == 0 && j == SquareSize.Y - 1)
+					{
+						tileSetAtlasCoordinates = new Vector2I(3, 3);
+						tileSetSource = 5;
+					}
+				
+					if (isStepDownNorthEast && i == SquareSize.X - 1 && j == 0)
+					{
+						tileSetAtlasCoordinates = new Vector2I(4, 2);
+						tileSetSource = 5;
+					}
+
+					if (isStepDownSouthEast && i == SquareSize.X - 1 && j == SquareSize.Y - 1)
+					{
+						tileSetAtlasCoordinates = new Vector2I(4, 3);
+						tileSetSource = 5;
+					}
+				}
+				
+				
+				SetCell(new Vector2I(worldPos.X+i, worldPos.Y+j), tileSetAtlasCoordinates, tileSetSource);	
+			}
 		}
 	}
 
-	private void SetCell(Vector2I tileMapPosition, int valueTier, bool isFrontier = false)
+	private void SetCell(Vector2I tileMapPosition, Vector2I tileSetAtlasCoordinates, int tileSetSourceId = 9)
 	{
 		var tileMapLayer = 0;
-		var tileSetSourceId = 9;
-		var tileSetAtlasCoordinates = new Vector2I(valueTier, 0);	// aqui calculamos si somos frontera y en qué dirección
-		_tileMap.SetCell(tileMapLayer, new Vector2I(tileMapPosition.X, tileMapPosition.Y), tileSetSourceId, tileSetAtlasCoordinates);
+		_tileMap.SetCell(tileMapLayer, new Vector2I(tileMapPosition.X, tileMapPosition.Y), tileSetSourceId, 
+			tileSetAtlasCoordinates);
 	}
 	
 	// FRONTIER
-	private bool IsFrontier(Vector2I squarePos)
+	private bool IsSideBorder(Vector2I squarePos, int valueTier)
 	{
-		var valueTier = _world.GetValueTierAt(squarePos.X/SquareSize.X, squarePos.Y/SquareSize.X);
-		return IsFrontierUp(squarePos, valueTier) || IsFrontierDown(squarePos, valueTier)
-				|| IsFrontierRight(squarePos, valueTier) || IsFrontierLeft(squarePos, valueTier);
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X, squarePos.Y/SquareSize.Y - 1) ||
+		       valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X, squarePos.Y/SquareSize.Y + 1) ||
+		       valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X - 1, squarePos.Y/SquareSize.Y) ||
+		       valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X + 1, squarePos.Y/SquareSize.Y);
+	}
+	
+	private bool IsStepDownNorth(Vector2I squarePos, int valueTier)
+	{
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X, squarePos.Y/SquareSize.Y - 1);
+	}
+	
+	private bool IsStepDownSouth(Vector2I squarePos, int valueTier)
+	{
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X, squarePos.Y/SquareSize.Y + 1);
+	}
+	
+	private bool IsStepDownWest(Vector2I squarePos, int valueTier)
+	{
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X - 1, squarePos.Y/SquareSize.Y);
+	}
+	
+	private bool IsStepDownEast(Vector2I squarePos, int valueTier)
+	{
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X + 1, squarePos.Y/SquareSize.Y);
 	}
 
-	private bool IsFrontierUp(Vector2I squarePos, int valueTier)
+	private bool IsStepDownNorthWest(Vector2I squarePos, int valueTier)
 	{
-		return valueTier != _world.GetValueTierAt(squarePos.X/SquareSize.X, squarePos.Y/SquareSize.Y - 1);
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X - 1, squarePos.Y/SquareSize.Y - 1);
 	}
 	
-	private bool IsFrontierDown(Vector2I squarePos, int valueTier)
+	private bool IsStepDownNorthEast(Vector2I squarePos, int valueTier)
 	{
-		return valueTier != _world.GetValueTierAt(squarePos.X/SquareSize.X, squarePos.Y/SquareSize.Y + 1);
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X + 1, squarePos.Y/SquareSize.Y - 1);
 	}
 	
-	private bool IsFrontierRight(Vector2I squarePos, int valueTier)
+	private bool IsStepDownSouthWest(Vector2I squarePos, int valueTier)
 	{
-		return valueTier != _world.GetValueTierAt(squarePos.X/SquareSize.X + 1, squarePos.Y/SquareSize.Y);
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X - 1, squarePos.Y/SquareSize.Y + 1);
 	}
-	
-	private bool IsFrontierLeft(Vector2I squarePos, int valueTier)
+
+	private bool IsStepDownSouthEast(Vector2I squarePos, int valueTier)
 	{
-		return valueTier != _world.GetValueTierAt(squarePos.X/SquareSize.X - 1, squarePos.Y/SquareSize.Y);
+		return valueTier > _world.GetValueTierAt(squarePos.X/SquareSize.X + 1, squarePos.Y/SquareSize.Y + 1);
 	}
 }
